167ce631c4f861792f82f56e74f8757354dfe0c6
Alex Kenan <alexkenan@users.noreply.github.com>
2017-12-13 19:14:02 -0500

    Create gitcommit.sh

---
+++ gitcommit/gitcommit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+. /Users/Alex/.bash_profile
+
+message="5 day commit $(date)"
+
+# Python3 directory
+cd /Users/Alex/Documents/Python3/
+echo "Directory: $(pwd)"
+/usr/local/bin/git add .
+/usr/local/bin/git commit -m "$message"
+/usr/local/bin/git push
+
+# Python directory
+cd /Users/Alex/Documents/Python/
+echo "Directory: $(pwd)"
+/usr/local/bin/git add .
+/usr/local/bin/git commit -m "$message"
+/usr/local/bin/git push
+
+# PythonGUI directory
+cd /Users/Alex/Documents/PythonGUI/
+echo "Directory: $(pwd)"
+/usr/local/bin/git add .
+/usr/local/bin/git commit -m "$message"
+/usr/local/bin/git push
