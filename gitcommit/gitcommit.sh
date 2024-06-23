a249e2f85553043ac23bd63773dedb45793c60f3
Alex Kenan <alexkenan@users.noreply.github.com>
2024-06-23 17:37:27 -0400

    Update gitcommit.sh

--- gitcommit/gitcommit.sh
+++ gitcommit/gitcommit.sh
@@ -1,35 +1,14 @@
-#!/bin/bash
-. /Users/Alex/.bash_profile
+#!/bin/zsh
+. /Users/alex/.zshrc
 
-message="5 day commit $(date)"
+message="Commit $(date)"
 
 # Save the .bash_profile alias file
-cp /Users/Alex/.bash_profile /Users/Alex/Documents/Bash/gitcommit/bash_profile.txt
+cp /Users/alex/.zshrc /Users/alex/Documents/Bash/gitcommit/zshrc.txt
 
 # Python3 directory
-cd /Users/Alex/Documents/Python3/
+cd /Users/alex/Documents/Python3/
 echo "Directory: $(pwd)"
-/usr/local/bin/git add .
-/usr/local/bin/git commit -m "$message"
-/usr/local/bin/git push
-
-# Python directory
-cd /Users/Alex/Documents/Python/
-echo "Directory: $(pwd)"
-/usr/local/bin/git add .
-/usr/local/bin/git commit -m "$message"
-/usr/local/bin/git push
-
-# PythonGUI directory
-cd /Users/Alex/Documents/PythonGUI/
-echo "Directory: $(pwd)"
-/usr/local/bin/git add .
-/usr/local/bin/git commit -m "$message"
-/usr/local/bin/git push
-
-# LaTeX directory
-cd /Users/Alex/Documents/LaTeX/
-echo "Directory: $(pwd)"
-/usr/local/bin/git add .
-/usr/local/bin/git commit -m "$message"
-/usr/local/bin/git push
+git add .
+git commit -m "$message"
+git push
